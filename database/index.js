148b2b8f1e2a2f49f2ac705df74f07318ebcedd6
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2024-05-30 00:32:34 +0530

    feat: Add user signup functionality
    The code changes include adding a new route `/signup` in the Express server to handle user signup requests. The route validates the input data, checks if the user already exists in the database, and saves the user data if it's a new user. This addition improves the functionality of the application by allowing users to create an account and store their information securely.

--- database/index.js
+++ database/index.js
@@ -1,14 +1,41 @@
+
+const express = require("express")
+const app = express()
+
+app.use(express.json());
+
 const mongoose = require("mongoose");
 mongoose.connect("mongodb+srv://baruah:arU7UaM7HFnKU69w@cluster0.0mb66ja.mongodb.net/userappnew")
-try{
 const User = mongoose.model('Users',{name:String ,email:String,password: String});
-const user = new User({
-    name:"Ratnadeep",
-    email:"test@gmail.com",
-    password:"123456",
-}) ;
-user.save();
-}
-catch(err){
-    console.log(err);
-}
+
+
+
+//CRUD OPERATION
+
+
+app.post('/signup',async function(req,res){
+    const username = req.body.username;
+    const password = req.body.password;
+    const name = req.body.name;
+ 
+    const existUser = await User.findOne({email:username})
+
+    if(existUser){
+        return res.status(403).json({
+            message:"User already exists"
+        })
+    }
+    
+
+    const user = new User({
+        name:name,
+        email:username, 
+        password:password,
+    }) ;
+    user.save();
+    res.json({
+        message:"User created successfully"
+    })
+})
+
+app.listen(3000)
