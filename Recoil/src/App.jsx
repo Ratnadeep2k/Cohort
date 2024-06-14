1327de1528e9201c0fb9d2aa5987aeb4c55cfe40
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2024-06-14 12:30:18 +0530

    chore: Add jobs, messaging, and notification atoms to App component

--- Recoil/src/App.jsx
+++ Recoil/src/App.jsx
@@ -1,7 +1,7 @@
 
 import './App.css'
-import { networkAtom } from './atoms'
-import { RecoilRoot, useRecoilValue } from 'recoil'
+import { jobsAtom, messagingAtom, networkAtom, notificationAtom } from './atoms'
+import { RecoilRoot, useRecoilValue,useRecoilState } from 'recoil'
 
 
 function App(){
@@ -13,14 +13,21 @@ function App(){
 
 function MainApp() {
  const networkNotificationcount = useRecoilValue(networkAtom)
+ const jobsAtomCout  = useRecoilValue(jobsAtom)
+ const notificationAtomCount = useRecoilValue(notificationAtom)
+ const [messagingAtomCount,setMessagingAtomCount] = useRecoilState(messagingAtom);
+
 
   return (
     <>
     <button>Home</button>
     <button>My Network({networkNotificationcount>100?"99+": networkNotificationcount })</button>
-    <button>Jobs ()</button>
-    <button>Messaging ()</button>
-    <button>Notifications ()</button>
+    <button>Jobs ({jobsAtomCout})</button> 
+    <button>Messaging ({messagingAtomCount})</button>
+    <button>Notifications ({notificationAtomCount})</button>
+    <button onClick={()=>{
+      setMessagingAtomCount (messagingAtomCount+1);
+    }} >Me</button>
     </>
   )
 }
