96f1c1157216513342946567d258b4733d4f80c2
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2024-09-15 13:43:42 +0530

    refactor: Update PostgreSQL connection string and add user insertion in index.ts

--- postgress/src/index.ts
+++ postgress/src/index.ts
@@ -6,17 +6,37 @@
    connectionString :"postgresql://postgres:ratnadeep@localhost:5432/postgres?sslmode=disable"
  })
 
- async function createUserTable(){
-    await client.connect()
-    const result = await client.query(`
-            CREATE TABLE users (
-                id SERIAL PRIMARY KEY,
-                username VARCHAR(50) UNIQUE NOT NULL,
-                email VARCHAR(50) UNIQUE NOT NULL,
-                password VARCHAR(50) NOT NULL,
-                created_at TIMESTAMP DEFAULT NOW()
-            );
-     `)
-     console.log(result);
- }
- createUserTable();
+//  async function createUserTable(){
+//     await client.connect()
+//     const result = await client.query(`
+//             CREATE TABLE users (
+//                 id SERIAL PRIMARY KEY,
+//                 username VARCHAR(50) UNIQUE NOT NULL,
+//                 email VARCHAR(50) UNIQUE NOT NULL,
+//                 password VARCHAR(50) NOT NULL,
+//                 created_at TIMESTAMP DEFAULT NOW()
+//             );
+//      `)
+//      console.log(result);
+//  }
+     async function insertData(){
+      await client.connect();
+      try{
+        const insertQuery ="INSERT INTO users (username,email,password) VALUES ('username2','username@gmail.com','anypassword')";
+        const result = await client.query(insertQuery);
+        console.log("Insertion Sucess",result);
+      }
+      catch(err){
+        console.log("Error",err);
+      }
+      finally{
+        await client.end();
+      }
+     }
+insertData();
+
+
+
+
+
+//  createUserTable();
