a65de99a094b70ca677d94ad0126c117a51e73b2
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2024-09-15 13:53:17 +0530

    refactor: Update PostgreSQL connection string and modify user insertion in index.ts

--- postgress/src/index.ts
+++ postgress/src/index.ts
@@ -19,11 +19,12 @@
 //      `)
 //      console.log(result);
 //  }
-     async function insertData(){
+     async function insertData(username:string,email:string,password :string){
       await client.connect();
       try{
-        const insertQuery ="INSERT INTO users (username,email,password) VALUES ('username2','username@gmail.com','anypassword')";
-        const result = await client.query(insertQuery);
+        const insertQuery ="INSERT INTO users (username,email,password) VALUES ($1,$2,$3)";
+        const values =[username ,email,password];
+        const result = await client.query(insertQuery,values);
         console.log("Insertion Sucess",result);
       }
       catch(err){
@@ -33,7 +34,7 @@
         await client.end();
       }
      }
-insertData();
+insertData("test","test@gmail.com","test123");
 
 
 
