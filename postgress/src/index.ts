9ccbdaecd5ad1dba47fb988b6607bd428fa2150e
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2024-09-15 13:59:21 +0530

    refactor: Update PostgreSQL connection string and modify user insertion in index.ts

--- postgress/src/index.ts
+++ postgress/src/index.ts
@@ -19,25 +19,43 @@
 //      `)
 //      console.log(result);
 //  }
-     async function insertData(username:string,email:string,password :string){
-      await client.connect();
-      try{
-        const insertQuery ="INSERT INTO users (username,email,password) VALUES ($1,$2,$3)";
-        const values =[username ,email,password];
-        const result = await client.query(insertQuery,values);
-        console.log("Insertion Sucess",result);
-      }
-      catch(err){
-        console.log("Error",err);
-      }
-      finally{
-        await client.end();
-      }
-     }
-insertData("test","test@gmail.com","test123");
 
 
-
-
-
+//Insert into database
+//      async function insertData(username:string,email:string,password :string){
+//       await client.connect();
+//       try{
+//         const insertQuery ="INSERT INTO users (username,email,password) VALUES ($1,$2,$3)";
+//         const values =[username ,email,password];
+//         const result = await client.query(insertQuery,values);
+//         console.log("Insertion Sucess",result);
+//       }
+//       catch(err){
+//         console.log("Error",err);
+//       }
+//       finally{
+//         await client.end();
+//       }
+//      }
+// insertData("test","test@gmail.com","test123");
 //  createUserTable();
+
+
+//Fetch email from db 
+
+
+async function getUser(email:string){
+  await client.connect();
+  const query  ='SELECT * FROM users WHERE email =$1';
+  const result = await client.query(query,[email]);
+
+  if(result.rows.length>0){
+    console.log(result.rows[0]);
+    return result.rows[0]; 
+  }
+  else {
+    console.log("No user found");
+    return null;
+  }
+} 
+getUser("username@gmail.com");
